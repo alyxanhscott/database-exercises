ea35bcd9673b546324273e7f5fa5e3def643dfcb
alyxandriascott <alyxan.hope26@gmail.com>
2018-03-16 16:14:22 -0500

    updated basic statements exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,9 +5,26 @@ CREATE TABLE albums (
   artist VARCHAR(50) NOT NULL,
   name VARCHAR(100) NOT NULL,
   release_date INT UNSIGNED NOT NULL DEFAULT 0,
-  sales FLOAT UNSIGNED NOT NULL DEFAULT 0.0,
   genre VARCHAR(50) NOT NULL,
+  sales FLOAT UNSIGNED NOT NULL DEFAULT 0.0,
   PRIMARY KEY (id)
 );
 
+CREATE TABLE contacts (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  number VARCHAR(50) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL,
+  PRIMARY KEY (id)
+);
+
+DESCRIBE contacts;
+
+INSERT INTO contacts (name, number, email, created_at,updated_at)
+VALUES ('tristan', '1234567895', 'tht@gmail.com', now(), now()),
+  ('allie', '6067488634','aaa@gmail.com', now(), now());
+
+SELECT * from contacts;
 # mysql -u codeup_test_user -p < albums_migration.sql
